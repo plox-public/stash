f92d1f119477b755bf450b0443c5939f56f535c4
stiles <plox.stiles@pm.me>
2025-12-18 08:08:38 -0600

    edit: reformat/organize config. add new way to change buffers

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,17 @@
 vim.g.mapleader = ' '
-vim.keymap.set('n', '<leader>bn', ':buffernext<CR>', {noremap=true})
-vim.keymap.set('n', '<leader>bp', ':bufferprev<CR>', {noremap=true})
+
+-- buffers and navigation
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', {noremap=true})
+vim.keymap.set('n', '<leader>bp', ':bprev<CR>', {noremap=true})
+vim.keymap.set('n', '<S-l>', ':bnext<CR>', {noremap=true})
+vim.keymap.set('n', '<S-h>', ':bnext<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>ls', ':ls<CR>', {noremap=true})
+vim.keymap.set('n', '<leader>e', ':Ex<CR>', {noremap=true})
+
+-- save/quit shortcuts
 vim.keymap.set('n', '<leader>ws', '<C-w>s<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>wv', '<C-w>v<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>wq', ':write<CR>:quit<CR>', {noremap=true})
-vim.keymap.set('n', '<leader>e', ':Ex<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>q', ':quit<CR>', {noremap=true})
 
 vim.pack.add{
---
+++ nvim/readme.md
@@ -0,0 +1,9 @@
+# plox-nvim
+
+Personal neovim configuration. Still trying to focus on minimalism and using 
+standard features only.
+
+## Dependencies
+
+Besides compiling neovim, will need to install the language servers that are
+defined in the configuration.
