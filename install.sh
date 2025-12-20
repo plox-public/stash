6c83a707515a24f2531a468690126163b5f93ff7
stiles <plox.stiles@pm.me>
2025-12-20 00:04:18 -0600

    check if nvim config exists and asks to overwrite

--- install.sh
+++ install.sh
@@ -6,7 +6,14 @@ if [[ ! -d "$HOME/.config" ]]; then
     mkdir -vp $HOME/.config;
 fi
 
-cp -r ./nvim "$HOME/.config/nvim"
+if [[ -d $HOME/.config/nvim ]]; then
+    echo "replace nvim config? y/n"
+    read resp
+    if [ $resp = "y" ]; then
+        rm -rf $HOME/.config/nvim
+        cp -r ./nvim "$HOME/.config/nvim"
+    fi
+fi
 
 ./gitconfig.sh
 ./get-nvim.sh
