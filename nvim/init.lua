ff933f4fa64cadd849f40c7156c7ecdd0920089d
stiles <plox.stiles@pm.me>
2025-12-31 14:10:55 -0600

    fix: buffer prev keybind fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,7 +3,7 @@ vim.g.mapleader = ' '
 -- buffers and navigation
 vim.keymap.set('n', '<leader>bn', ':bnext<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>bp', ':bprev<CR>', {noremap=true})
-vim.keymap.set('n', '<S-l>', ':bnext<CR>', {noremap=true})
+vim.keymap.set('n', '<S-l>', ':bprev<CR>', {noremap=true})
 vim.keymap.set('n', '<S-h>', ':bnext<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>ls', ':ls<CR>', {noremap=true})
 vim.keymap.set('n', '<leader>e', ':Ex<CR>', {noremap=true})
